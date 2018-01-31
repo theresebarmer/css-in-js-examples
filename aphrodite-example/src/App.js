34eb6018a45e3ef92d0eafea103f415c351be81e
Therése Barmer <therese.barmer@gmail.com>
2018-01-31 22:37:14 +0100

    Rename title

--- aphrodite-example/src/App.js
+++ aphrodite-example/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StyleSheet, css } from 'aphrodite';
+
+
 
 class App extends Component {
   render() {
