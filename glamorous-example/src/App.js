e1d94982632367a82327d277ce47955dc546927b
Therése Barmer <therese.barmer@gmail.com>
2018-02-01 10:56:13 +0100

    Add wrapper for button

--- glamorous-example/src/App.js
+++ glamorous-example/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import glamorous from 'glamorous';
 
+const Wrapper = glamorous.section({
+    textAlign: 'center',
+    padding: '4em'
+});
+
 const Button = glamorous.button({
     backgroundColor: 'green',
     '&:hover': {
@@ -12,7 +17,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <Wrapper>
           <Button>Click me!</Button>
+          </Wrapper>
       </div>
     );
   }
